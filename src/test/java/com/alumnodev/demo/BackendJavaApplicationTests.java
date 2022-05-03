128d5ef8a710280d6df1c83d67133a6f0207451e
acotto-san <acostaottonelli.santiago@gmail.com>
2022-05-02 22:11:00 -0300

    primer commit

---
+++ src/test/java/com/alumnodev/demo/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alumnodev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
